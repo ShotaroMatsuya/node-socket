0d4c13f9e9e812effcbfd39627e798db82025f09
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-09-27 00:32:34 +0900

    implement CRUD

--- BBBBB/controllers/feed.js
+++ BBBBB/controllers/feed.js
@@ -1,38 +1,46 @@
+const fs = require('fs');
+const path = require('path');
 const {
   validationResult
 } = require('express-validator/check');
 const Post = require('../models/post');
+const post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{
-      _id: '1',
-      title: 'First Post',
-      content: 'This is the first post!',
-      imageUrl: 'images/duck.jpg',
-      creator: {
-        name: 'Shotaro',
-      },
-      createdAt: new Date(),
-    }, ],
+  Post.find().then(posts => {
+    res.status(200).json({
+      message: 'Fetched posts successfully.',
+      posts: posts
+    });
+
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); //asynchronous function内ではnextでerrを渡す
   });
+
 };
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array(),
-      });
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error; //synchronous function内ではthrow
+
   }
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/duck.jpg',
+    imageUrl: imageUrl,
     creator: {
       name: 'Shotaro'
     },
@@ -44,7 +52,107 @@ exports.createPost = (req, res, next) => {
       post: result
     });
   }).catch(err => {
-    console.log(err)
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); //asynchronous function内ではnextでerrを渡す
   });
+
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find  post.');
+      error.statusCode = 404;
+      throw error; //asynchronous でもthenブロックの中ではthrowを使ってcatchブロックにわたす
+    }
+    res.status(200).json({
+      message: 'Post fetched.',
+      post: post
+    });
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); //asynchronous function内ではnextでerrを渡す
+  });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error; //synchronous function内ではthrow
+
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image; //すでにセットされているurl
+  if (req.file) { //新たに追加されたfile
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked.');
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find  post.');
+      error.statusCode = 404;
+      throw error; //asynchronous でもthenブロックの中ではthrowを使ってcatchブロックにわたす
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+    post.title = title;
+    post.imageUrl = imageUrl;
+    post.content = content;
+    return post.save();
+  }).then(result => {
+    res.status(200).json({
+      message: 'Post updated!',
+      post: result
+    });
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); //asynchronous function内ではnextでerrを渡す
+  });
+
+};
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find  post.');
+      error.statusCode = 404;
+      throw error; //asynchronous でもthenブロックの中ではthrowを使ってcatchブロックにわたす
+    }
+    //check logged in user
+    clearImage(post.imageUrl);
+    return Post.findByIdAndRemove(postId);
+  }).then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: 'Deleted post.'
+    });
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err); //asynchronous function内ではnextでerrを渡す
+  });
+};
+
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 
 };
